1f58d14c129ef8c99566fda01902b48450ab58db
rahulkrishnan221 <rahulkrishnan221@gmail.com>
2018-03-17 15:40:52 +0530

    Grant the access

--- src/table_read.java
+++ src/table_read.java
@@ -11,33 +11,43 @@ import java.util.Map;
 
 public class table_read {
     public static void main(String arg[])throws IOException
-    {Connection.Response res = Jsoup
+    {
+        Connection.Response res = Jsoup
             .connect("http://evarsity.srmuniv.ac.in/srmswi/usermanager/ParentLogin.jsp")
-            .data("txtSN", "PRA1611003040056", "txtPD", "07061998")
-            .method(Connection.Method.POST)
+            .data("txtRegNumber","iamalsouser")
+                .data("txtPwd","thanksandregards")
+                .data("txtSN","PRA1611003040056")
+                .data("txtPD","07061998")
+                .data("txtPA","1")
+            .method(Connection.Method.GET)
             .execute();
 
 //This will get you cookies
         Map<String, String> cookies = res.cookies();
 
         ArrayList<String> downServers = new ArrayList<>();
-        Document doc1=Jsoup.connect("https://www.w3schools.com/tags/tag_table.asp").get();
-        Document doc = Jsoup.connect("http://evarsity.srmuniv.ac.in/srmswi/resource/StudentDetailsResources.jsp?resourceid=7").cookies(cookies).get();
-        Element table = doc.select("table").get(0); //select the first table.
-        Elements rows = table.select("tr");
+       // Document doc1=Jsoup.connect("https://www.w3schools.com/tags/tag_table.asp").get();
+     //   Document doc = Jsoup.connect("http://evarsity.srmuniv.ac.in/srmswi/resource/StudentDetailsResources.jsp?resourceid=7").cookies(cookies).get();
 
-        Iterator<Element> iterator = table.select("td").iterator();
+        Document doc=Jsoup.connect("http://evarsity.srmuniv.ac.in/srmswi/resource/StudentDetailsResources.jsp?resourceid=7").cookies(cookies).get();
+       Element table = doc.select("table").get(0); //select the first table.
+       // System.out.println(table);
+     //   Elements rows = table.select("tr");
+
+        Iterator<Element> iterator = table.select("tr").iterator();
         while(iterator.hasNext()){
             System.out.println(iterator.next().text()); //kolom -1
-            System.out.println(iterator.next().text()); //kolom -2
+            
         }
 
-        for (int i = 1; i < rows.size(); i++) { //first row is the col names so skip it.
+      /*  for (int i = 1; i < rows.size(); i++) { //first row is the col names so skip it.
             Element row = rows.get(i);
             Elements cols = row.select("td");
           System.out.println(cols);
 
 
         }
+        */
+
     }
 }
